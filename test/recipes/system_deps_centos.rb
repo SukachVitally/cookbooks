60a98200314f5eb89334c3052db9eed81ac61efb
Sukach <sukach.v.v@gmail.com>
2015-11-12 12:09:24 +0200

    fix

--- test/recipes/system_deps_centos.rb
+++ test/recipes/system_deps_centos.rb
@@ -3,7 +3,7 @@
 # Recipe:: system_deps_centos
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
-include_recipe 'build-essential::default'
+# include_recipe 'build-essential::default'
 include_recipe 'python::pip'
 include_recipe 'python::virtualenv'
 
